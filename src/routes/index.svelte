e0bf0576f50bbb34783b38214be9c75e9629fbae
Mehdi El Fadil <m.ef@mango-is.com>
2022-03-25 17:27:27 +0100

    start writing home page content #2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<article class="prose lg:prose-xl">
+	
+	<h2 >What's the next step for your business?</h2>
+	<h1>Let's work together and unveil actionable insights from your data.</h1>
+	
+	<p>Mehdi is a Business Intelligence specialist working for more than 15 years on analytics, data visualization and data integration projects.</p>
+	<p>Mehdi helps guide business managers through their data analysis and technology challenges. With sound business experience, he talks your language.</p>
+
+	<p><a href="/contact">Contact Mehdi</a></p>
+
+	<ul>
+		<li>Contracting authority support</li>
+		<li>Strategic consulting</li>
+		<li>Business analysis</li>
+		<li>Technical experts in complex data-intensive projects</li>
+</ul>
+
+</article>
