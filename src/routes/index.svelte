ac2d8db33a0d98610da313d68a54b7b984e8e882
Mehdi El Fadil <m.ef@mango-is.com>
2022-06-07 15:05:13 +0100

    add client logos #2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,18 @@
 
 	<p><a href="/contact">Contact Mehdi</a></p>
 
+
+	<img class="inline-block" src="/img/reference/bpost.png" alt="bPost client logo"/>
+	<img class="inline-block" src="/img/reference/deloitte.png" alt="Deloitte client logo"/>
+	<img class="inline-block" src="/img/reference/eeip.png" alt="EEIP client logo"/>
+	<img class="inline-block" src="/img/reference/emolytics.png" alt="Emolytics client logo"/>
+	<img class="inline-block" src="/img/reference/european-commission.png" alt="European Commission client logo"/>
+	<img class="inline-block" src="/img/reference/international-post-corporation.png" alt="International Post Corporation client logo"/>
+	<img class="inline-block" src="/img/reference/smoall.png" alt="Smoall client logo"/>
+	<img class="inline-block" src="/img/reference/unicef.png" alt="Unicef client logo"/>
+	<img class="inline-block" src="/img/reference/universal-music-group.png" alt="Universal-music-group client logo"/>
+	<img class="inline-block" src="/img/reference/vrt.png" alt="VRT client logo"/>
+
 	<ul>
 		<li>Contracting authority support</li>
 		<li>Strategic consulting</li>
